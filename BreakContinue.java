03b67070839f15a1ed96a4e34aab26029c933c00
Manoj Kumar <mk.guggilam@gmail.com>
2018-03-08 03:47:51 +0530

    Added break program

---
+++ BreakContinue.java
@@ -0,0 +1,23 @@
+
+public class BreakContinue {
+
+	public static void main(String[] args) {
+		
+		/*for(int i=0;i<10;i++) {
+			if(i>=5 && i<=7) {
+				
+				continue;
+				
+			}
+			System.out.println(i);
+		}*///example for continue statement
+		int First = 10;
+		int Second = 20;
+		boolean result = First>Second;//example for boolean stmt
+		System.out.println(result);
+		
+		System.out.println("Out");
+
+	}
+
+}
